43d30bdb5811ab2fde60eaf9341cb42356fd23db
Sebastian Mojado <sebastian.mojado@gmail.com>
2022-08-22 17:25:10 +0200

    Added easy sample with a JSON from the demo account

---
+++ samples/json-to-csv/ConverterUsageSample/Program.cs
@@ -0,0 +1,27 @@
+﻿// To use the converter it is necessary to have: 
+//    - .NET6
+//    - the converter library (myCalibration.Converters.dll)
+//    - Newtonsoft.Json nuget v13.0.1 https://www.nuget.org/packages/Newtonsoft.Json/
+
+
+namespace ConverterUsageSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonText = File.ReadAllText("sample-data/PAA-9L_123021.1804_43345.json");
+
+            KellerSensorDataExchange.KellerSensorData dataStoredInJsonFile = myCalibration.Converters.ConvertJsonToObject.Convert(jsonText);
+            Console.WriteLine("This is the data from a " + dataStoredInJsonFile.Header.ProductType + " with serial number " + dataStoredInJsonFile.Header.SerialNumber + Environment.NewLine);
+
+            myCalibration.Converters.data_model.CalibrationDataAsTexts convertedData = myCalibration.Converters.MyCalibrationJsonConvert.JsonTextToAll(jsonText, separator:',');
+
+            Console.WriteLine("The measurement data (Testrun) : " + Environment.NewLine + convertedData.MeasurementsTxtText + Environment.NewLine);
+            Console.WriteLine("The MathMod coefficient data   : " + Environment.NewLine + convertedData.CoefficientsTxtText + Environment.NewLine);
+
+            Console.WriteLine("The measurement data (Testrun) as CSV : " + Environment.NewLine + convertedData.MeasurementsCsvText + Environment.NewLine);
+            Console.WriteLine("The MathMod coefficient data   as CSV : " + Environment.NewLine + convertedData.CoefficientsCsvText + Environment.NewLine);
+        }
+    }
+}
